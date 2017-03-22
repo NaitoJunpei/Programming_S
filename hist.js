f2cd63155393530c3c741730f0cc059daf89d90a
NaitoJunpei <nighty1919@gmail.com>
2017-03-23 01:13:20 +0900

    ちょっとレイアウト変更　カッパをランダムに決定

--- hist.js
+++ hist.js
@@ -22,9 +22,9 @@ function GenerateRandomSpikes( ) {
     }
     
     var kappa = new Array(3)
-    kappa[0] = 5.0; kappa[1] = 2.0; kappa[2] = 0.8;
+    kappa[0] = 5.0; kappa[1] = MT.next() * (2.0 - 0.75) + 0.75; kappa[2] = 0.8;
 
-    
+    console.log(kappa[1]);
     for (var i = 0; i < 3; i++) {
 	SpikeData[i] = new Array();
 	GenerateSpikes(i, kappa[i], SpikeData[i]);
@@ -43,7 +43,7 @@ function GenerateRandomSpikes( ) {
 //////////////////////////////////////////////////
 var Base=30.0;
 var Amplitude=10.0;
-var TIME=30.0;
+var TIME=10.0;
 var Period=[2.0/Math.PI,1.41421356/Math.PI,0.8989898/Math.PI];
 
 function Rate( index, t ){
@@ -135,6 +135,11 @@ function GenerateSpikes(index, kappa, spike_time){
 
 }
 
+function Initialize() {
+    document.data.spikes.value = "0.017 0.058 0.119 0.191 0.194 0.259 0.289 0.305 0.346 0.355 0.481 0.537 0.566 0.621 0.642 0.703 0.738 0.8 1.035 1.059 1.097 1.158 1.297 1.339 1.475 1.495 1.603 1.634 1.663 1.771 1.789 1.801 1.835 1.887 1.91 1.914 1.975 2 2.023 2.041 2.059 2.081 2.112 2.155 2.166 2.195 2.214 2.242 2.258 2.264 2.329 2.343 2.354 2.384 2.395 2.411 2.438 2.45 2.47 2.485 2.503 2.529 2.56 2.582 2.587 2.596 2.6 2.623 2.628 2.642 2.661 2.7 2.71 2.717 2.734 2.738 2.758 2.775 2.775 2.786 2.797 2.809 2.838 2.853 2.861 2.866 2.877 2.906 2.958 2.977 3.012 3.031 3.039 3.086 3.092 3.101 3.121 3.156 3.168 3.189 3.19 3.222 3.254 3.275 3.277 3.329 3.331 3.35 3.386 3.39 3.44 3.455 3.518 3.519 3.551 3.617 3.648 3.651 3.668 3.705 3.715 3.793 3.808 3.832 3.858 3.889 3.898 3.969 4.053 4.144 4.194 4.407 4.523 4.578 4.643 4.676 4.705 4.803 4.917 5.022 5.056 5.095 5.127 5.164 5.244 5.286 5.339 5.401 5.431 5.485 5.491 5.615 5.636 5.65 5.658 5.687 5.736 5.746 5.797 5.854 5.915 5.933 5.96 6.075 6.152 6.179 6.206 6.27 6.288 6.441 6.489 6.505 6.512 6.53 6.556 6.583 6.614 6.64 6.652 6.689 6.733 6.79 6.795 6.799 6.864 6.886 6.891 6.894 6.917 6.941 7.007 7.02 7.027 7.061 7.081 7.089 7.112 7.128 7.145 7.152 7.177 7.183 7.213 7.221 7.227 7.243 7.26 7.263 7.298 7.333 7.351 7.373 7.38 7.4 7.419 7.435 7.475 7.497 7.512 7.529 7.54 7.582 7.589 7.59 7.707 7.733 7.805 7.815 7.845 7.911 7.939 7.963 8.021 8.037 8.052 8.059 8.083 8.119 8.159 8.185 8.264 8.295 8.383 8.499 8.605 8.689 8.724 8.789 8.855 8.874 8.901 8.909 8.919 9.004 9.047 9.065 9.073 9.096 9.115 9.147 9.204 9.228 9.244 9.259 9.294 9.332 9.363 9.401 9.438 9.449 9.463 9.511 9.576 9.581 9.586 9.6 9.628 9.635 9.722 9.725 9.74 9.75 9.781 9.789 9.871 9.905 9.925 9.958";
+    Main();
+}
+
 function ResetData(){
     //document.data.spikes.value="0.012 0.050 0.066 0.078 0.113 0.145 0.159 0.188 0.206 0.226 0.261 0.280 0.294 0.305 0.330 0.349 0.368 0.412 0.448 0.477 0.512 0.524 0.529 0.547 0.565 0.588 0.595 0.607 0.619 0.645 0.664 0.685 0.699 0.721 0.737 0.766 0.779 0.814 0.830 0.842 0.872 0.883 0.892 0.903 0.917 0.935 0.959 0.974 0.981 0.995 1.017 1.052 1.067 1.094 1.108 1.124 1.139 1.164 1.183 1.201 1.216 1.234 1.256 1.272 1.291 1.311 1.319 1.344 1.395 1.419 1.436 1.475 1.484 1.527 1.551 1.597 1.614 1.642 1.664 1.725 1.755 1.815 1.854 1.925 1.988 2.127 2.323 2.372 2.515 2.589 2.616 2.713 2.765 2.808 2.837 2.895 2.925 2.943 2.973 3.008 3.040 3.073 3.132 3.145 3.165 3.175 3.195 3.219 3.236 3.256 3.273 3.296 3.301 3.317 3.329 3.335 3.350 3.359 3.376 3.390 3.412 3.439";
     
@@ -155,8 +160,9 @@ function Main(){
     optimal_binsize_g=OS(spike_time);
     optimal_binsize_p=SS(spike_time);
     
-    
-    DrawGraph(spike_time,optimal_binsize_g,optimal_binsize_p);
+    DrawGraphSS(spike_time, optimal_binsize_p);
+    DrawGraphOS(spike_time, optimal_binsize_g);
+    //DrawGraph(spike_time,optimal_binsize_g,optimal_binsize_p);
     
     /*for output*/
     OUTPUT_binsize_p=optimal_binsize_p;
@@ -245,140 +251,108 @@ var y_graph=140;
 var height_graph=130;
 var height_hist=120;
 
-function SpikeRaster(spike_time){
+function SpikeRaster(spike_time, canvas_id){
 
     var spike_num=spike_time.length;
     var onset  = spike_time[0]           - 0.001*(spike_time[spike_num-1]-spike_time[0]);
     var offset = spike_time[spike_num-1] + 0.001*(spike_time[spike_num-1]-spike_time[0]);
     
-    var canvas = document.getElementById('raster');
+    var canvas = document.getElementById(canvas_id);
     if ( ! canvas || ! canvas.getContext ) {	return false;	}
     var ctx = canvas.getContext('2d');
 	
     /*reset canvas*/
-    ctx.clearRect(0,0,800,70);
+    ctx.clearRect(0,y_graph,800,y_raster);
     
     /*raster*/
     ctx.strokeStyle = "black"
-    ctx.strokeRect(x_base-10,y_raster,width+20,0);
+    ctx.strokeRect(x_base-10,y_raster + y_graph,width+20,0);
 	
     for(i=0;i<spike_num;i++){
 	x=(spike_time[i]-onset)/(offset-onset);
 	ctx.strokeStyle = "black";
-	ctx.strokeRect(x_base+width*x,y_raster,0,-30);
+	ctx.strokeRect(x_base+width*x,y_raster + y_graph,0,-30);
     }
     
     /*text*/
-    ctx.font = "16px 'Arial'";
-    ctx.fillText("Spikes",10,y_raster-10);
+    //ctx.font = "16px 'Arial'";
+    //ctx.fillText("Spikes",10,y_raster-10);
     
     return 0;
 
 }
 
-function DrawGraph( spike_time, optimal_binsize_g, optimal_binsize_p ){
-	
-    var spike_num=spike_time.length;
-    var onset  = spike_time[0]           - 0.001*(spike_time[spike_num-1]-spike_time[0]);
-    var offset = spike_time[spike_num-1] + 0.001*(spike_time[spike_num-1]-spike_time[0]);
-    
-    var optimal_rate_p,optimal_rate_g;
-    var rate_max;
-    
-    optimal_rate_g = new Array();
-    rate_max = Estimate_Rate( spike_time, optimal_binsize_g, optimal_rate_g );
-    
-    optimal_rate_p = new Array();
-    if( rate_max < Estimate_Rate( spike_time, optimal_binsize_p, optimal_rate_p ) ) rate_max = Estimate_Rate( spike_time, optimal_binsize_p, optimal_rate_p );
-    
-    var canvas = document.getElementById('graph');
-    if ( ! canvas || ! canvas.getContext ) {	return false;	}
-    var ctx = canvas.getContext('2d');
-	
-    /*reset canvas*/
-    ctx.clearRect(0,0,800,200);
-	
-    /*histogram*/
-    ctx.strokeStyle = "black";
-    ctx.strokeRect(x_base,y_graph,width,-height_graph);
-	
-    /*0S*/
-    for(var i=0;i<optimal_rate_g.length;i++){
-		
-        var x=i*optimal_binsize_g/(offset-onset);
-	var y=optimal_rate_g[i]/rate_max;
-        
-        var xx=x_base+width*x;
-        var yy=height_hist*y;
-        
-        ctx.fillStyle="blue";
-		
-        if( onset + (i+1)*optimal_binsize_g < offset ) ctx.fillRect(xx,y_graph,width*optimal_binsize_g/(offset-onset),-yy);
-        else ctx.fillRect(xx,y_graph,width-width*x,-height_hist*y);
-    }
-    
-    /*SS*/
-    for(var i=0;i<optimal_rate_p.length;i++){
-		
-        var x=i*optimal_binsize_p/(offset-onset);
-	var y=optimal_rate_p[i]/rate_max;
-        
-        ctx.fillStyle="red";
-        
-        for(var xx=x_base+width*x; xx< x_base+width*(i+1)*optimal_binsize_p/(offset-onset) ; xx+=4 ) {
-            
-            if(xx > x_base+width ) break;
-            
-            var yy=y_graph-height_hist*y;
-            ctx.fillRect(xx,yy-1,2,2);
+function DrawHist( spike_time, optimal_binsize, canvas_id ) {
+    var spike_num = spike_time.length;
+    var onset = spike_time[0] - 0.001 * (spike_time[spike_num - 1] - spike_time[0]);
+    var offset = spike_time[spike_num - 1] + 0.001 * (spike_time[spike_num - 1] - spike_time[0]);
 
-        }
-        
-        /*horizontal*/
-        for(var xx=x_base+width*x; xx< x_base+width*(i+1)*optimal_binsize_p/(offset-onset) ; xx+=4 ) {
-            var yy=y_graph-height_hist*y;
-            ctx.fillRect(xx,yy-1,2,2);
-        }
-        
-        /*vertical*/
-        if( i < optimal_rate_p.length-1 ){
-        
-            if( optimal_rate_p[i] < optimal_rate_p[i+1] ){
-                for(var yy=y_graph-height_hist*y; yy>y_graph-height_hist*optimal_rate_p[i+1]/rate_max; yy-=4) {
-                    var xx=x_base+width*(i+1)*optimal_binsize_p/(offset-onset);
-                    ctx.fillRect(xx-1,yy,2,-2);
-                }
-            }
-        
-            else{
-                for(var yy=y_graph-height_hist*y; yy<y_graph-height_hist*optimal_rate_p[i+1]/rate_max; yy+=4) {
-                    var xx=x_base+width*(i+1)*optimal_binsize_p/(offset-onset);
-                    ctx.fillRect(xx-1,yy,2,2);
-                }
-            }
-        }   
-        
+    var optimal_rate;
+    var rate_max;
+
+    optimal_rate = new Array();
+    rate_max = Estimate_Rate(spike_time, optimal_binsize, optimal_rate);
+
+    var canvas = document.getElementById(canvas_id);
+    if (!canvas || !canvas.getContext) {return false;}
+    var ctx = canvas.getContext('2d');
+
+    /* reset canvas */
+    ctx.clearRect(0, 0, 800, y_graph);
+
+    /* histogram */
+    ctx.strokeStyle = "brack";
+    ctx.strokeRect(x_base, y_graph, width, -height_graph);
+
+    for(var i = 0; i < optimal_rate.length; i++) {
+	var x = i * optimal_binsize / (offset - onset);
+	var y = optimal_rate[i] / rate_max;
+
+	var xx = x_base + width * x;
+	var yy = height_hist * y;
+
+	ctx.fillStyle="blue";
+	if (onset + (i + 1) * optimal_binsize < offset) ctx.fillRect(xx, y_graph, width * optimal_binsize / (offset - onset), -yy);
+	else ctx.fillRect(xx, y_graph, width - width * x, -height_hist * y);
     }
+}
+
+function DrawGraphSS( spike_time, optimal_binsize ){
 	
-    ctx.font = "12px 'Arial'";
-    ctx.fillStyle = "black";
-    ctx.fillText((rate_max*height_graph/height_hist).toFixed(2),x_base-35,y_graph-height_graph);
-    ctx.fillText("0",x_base-20,y_graph);
-    ctx.fillText(spike_time[0].toFixed(2),x_base+5,y_graph+20);
-    ctx.fillText(spike_time[spike_num-1].toFixed(2),x_base+width+5,y_graph+20);
-    ctx.font = "16px 'Arial'";
-    ctx.fillText("time",x_base+0.5*width-20,y_graph+35);
-    ctx.fillText("Rate",20,y_graph-height_graph/2+10);
+    DrawHist(spike_time, optimal_binsize, "graph_SS");
+    SpikeRaster(spike_time, "graph_SS");
+
+    //ctx.font = "12px 'Arial'";
+    //ctx.fillStyle = "black";
+    //ctx.fillText((rate_max*height_graph/height_hist).toFixed(2),x_base-35,y_graph-height_graph);
+    //ctx.fillText("0",x_base-20,y_graph);
+    //ctx.fillText(spike_time[0].toFixed(2),x_base+5,y_graph+20);
+    //ctx.fillText(spike_time[spike_num-1].toFixed(2),x_base+width+5,y_graph+20);
+    //ctx.font = "16px 'Arial'";
+    //ctx.fillText("time",x_base+0.5*width-20,y_graph+35);
+    //ctx.fillText("Rate",20,y_graph-height_graph/2+10);
     
     /*output*/
     var np;
     if( Calc_lv(spike_time)<1 ) np="regular";
     else np="bursty";
     
-    document.getElementById("poisson").innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;The red dotted line represents a histogram constructed with the Poissonian optimization method (Shimazaki & Shinomoto, 2007).";
+    //document.getElementById("poisson").innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;The red dotted line represents a histogram constructed with the Poissonian optimization method (Shimazaki & Shinomoto, 2007).";
 	
-    document.getElementById('optimal').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;The optimal bin size is <font color=\"red\">" + optimal_binsize_g.toFixed(2) + "</font>.<br>&nbsp;&nbsp;&nbsp;&nbsp;The non-Poisson characteristic of your data is estimated by Lv as Lv = <font color=\"red\">" + Calc_lv(spike_time).toFixed(2) + "</font> (<font color=\"red\">" + np +"</font> firing).";
+    //document.getElementById('optimal').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;The optimal bin size is <font color=\"red\">" + optimal_binsize_g.toFixed(2) + "</font>.<br>&nbsp;&nbsp;&nbsp;&nbsp;The non-Poisson characteristic of your data is estimated by Lv as Lv = <font color=\"red\">" + Calc_lv(spike_time).toFixed(2) + "</font> (<font color=\"red\">" + np +"</font> firing).";
+    document.getElementById('optimal_SS').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;Shimazaki-Shinomoto histgram[1, <a href='./'> more details</a>]\n Optimal binsize = <font color=\"red\">" + optimal_binsize.toFixed(2) + "</font>."
 	
+}
+
+function DrawGraphOS(spike_time, optimal_binsize) {
+    DrawHist(spike_time, optimal_binsize, "graph_OS");
+    SpikeRaster(spike_time, "graph_OS");
+
+    /* output */
+    var np;
+    if( Calc_lv(spike_time)<1 ) np="regular";
+    else np="bursty";
+    document.getElementById('optimal_OS').innerHTML="&nbsp;&nbsp;&nbsp;&nbsp;"
 }
 
 ///////////////////////////////
